a68f7fff026f02bc1b19c71d6a2ef7fa7d023280
ectuser <m.ectuser@gmail.com>
2020-05-11 17:06:36 +0700

    Added expected value counter and other stuff

--- eventprobabilitytheory/src/components/DiscreteRandomVariable.js
+++ eventprobabilitytheory/src/components/DiscreteRandomVariable.js
@@ -3,11 +3,18 @@ import { Form, Row, Col, Container, Button } from 'react-bootstrap';
 import Title from "./Title"
 import { Bar } from 'react-chartjs-2';
 
-const initialState = {events : [], startButton : "", experimentsData : [], experimentsLabels : []};
+const initialState = {events : [], 
+    startButton : "", 
+    experimentsData : [], 
+    experimentsLabels : [],
+    expectedValue : 0,
+    variance : 0,
+    expectedValueError : 0,
+    varianceError : 0
+};
 
 const reducer = (state, action) => {
     if (action.type === "INIT_EMPTY_EVENTS"){
-        console.log(action.payload)
         return {
             ...state,
             events : [...action.payload]
@@ -20,11 +27,19 @@ const reducer = (state, action) => {
         }
     }
     else if (action.type === "SET_CHART_DATA"){
-        console.log(action.payload);
         return {
             ...state,
             experimentsData : [...action.payload.data],
             experimentsLabels : [...action.payload.labels]
+        }
+    }
+    else if (action.type === "SET_COUNTED_DATA"){
+        return {
+            ...state,
+            expectedValue : action.payload.expectedValue,
+            variance : action.payload.variance,
+            expectedValueError : action.payload.expectedValueError,
+            varianceError : action.payload.varianceError
         }
     }
 }
@@ -43,7 +58,8 @@ const DiscreteRandomVariable = () => {
                 newEvents.push({
                     name : `Event ${i}`,
                     ref : refs.current[i],
-                    amountOfSelections : 0
+                    amountOfSelections : 0,
+                    ferProbability : 0
                 })
             }
             console.log(newEvents);
@@ -84,6 +100,55 @@ const DiscreteRandomVariable = () => {
         return {data : data, labels : labels}
     }
 
+    const countExpectedValue = async (xs, ps) => {
+        let sum = 0;
+        for (let i = 0; i < xs.length; i++){
+            sum += xs[i] * ps[i];
+        }
+        return sum;
+    }
+    const countVariance = async (xs, ps, Ex) => {
+        let sum = 0;
+        for (let i = 0; i < xs.length; i++){
+            sum += (ps[i] * (xs[i] ** 2))
+        }
+        sum -=  (Ex ** 2);
+        return sum;
+    }
+
+    const roundTwoDecimal = (number) => Math.round((number + Number.EPSILON) * 100) / 100
+
+    const solve = async () => {
+        let xs = [];
+        let ps = [];
+        let realPs = [];
+        for (let i = 0; i < state.events.length; i++){
+            xs.push(i);
+            ps.push(Number(state.events[i].ref.value) / 100);
+            realPs.push(state.events[i].ferProbability);
+        }
+
+        let theoreticalEx = await countExpectedValue(xs, ps);
+        let theoreticalVariance = await countVariance(xs, ps, theoreticalEx);
+
+        let realEx = await countExpectedValue(xs, realPs);
+        let realVariance = await countVariance(xs, realPs, realEx);
+        console.log(theoreticalEx, theoreticalVariance, realEx, realVariance);
+
+        const absMeasurementExError = Math.abs(realEx - theoreticalEx);
+        const absMeasurementVarianceError = Math.abs(realVariance - theoreticalVariance);
+
+        const relativeMeasurementExError = absMeasurementExError / Math.abs(theoreticalEx);
+        const relativeMeasurementVarianceError = absMeasurementVarianceError / Math.abs(theoreticalVariance);
+
+        dispatch({type : "SET_COUNTED_DATA", payload : {
+            expectedValue : realEx,
+            variance : realVariance,
+            expectedValueError : relativeMeasurementExError,
+            varianceError : relativeMeasurementVarianceError
+        }})
+    }
+
     const start = async () => {
         if (! await areValuesCorrect()){
             alert("Values are incorrect");
@@ -108,13 +173,15 @@ const DiscreteRandomVariable = () => {
                 randomProbability -= Number(ev.ref.value);
                 if (randomProbability <= 0){
                     ev.amountOfSelections++;
+                    ev.ferProbability = ev.amountOfSelections / experimentsNumber;
                     break;
                 }
             }
         }
         let chartData = await getChartData(tempEvents, experimentsNumber);
         dispatch({type : "SET_CHART_DATA", payload : chartData});
-        
+
+        await solve();
     }
 
     return(
@@ -129,33 +196,36 @@ const DiscreteRandomVariable = () => {
                 </Row>
             ))}
             <div>
-                    <Form.Control ref={amountOfExperimentsInput} type="number" placeholder="Probability" />
-                    <Button className="m-2" variant="primary" onClick={start}>Start</Button>
-                    <Bar data={{
-                        datasets: [{
-                            data: state.experimentsData
-                        }],
-                        labels: state.experimentsLabels
-                    }} 
-                    options={{
-                        legend : {
-                            display : false
-                        },
-                        title: {
-                            display: true,
-                            text: 'Frequency'
-                        },
-                        scales: {
-                            yAxes: [{
-                                ticks: {
-                                    suggestedMin: 0,
-                                    suggestedMax: 1
-                                }
-                            }]
-                        }
+                <Form.Control ref={amountOfExperimentsInput} type="number" placeholder="Amount of experiments" />
+                <Button className="m-2" variant="primary" onClick={start}>Start</Button>
+                <Bar data={{
+                    datasets: [{
+                        data: state.experimentsData
+                    }],
+                    labels: state.experimentsLabels
+                }} 
+                options={{
+                    legend : {
+                        display : false
+                    },
+                    title: {
+                        display: true,
+                        text: 'Frequency'
+                    },
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                suggestedMin: 0,
+                                suggestedMax: 1
+                            }
+                        }]
                     }
-                 } />
-                </div>
+                }
+                } />
+                <div>Expected value : {roundTwoDecimal(state.expectedValue)} (error = {roundTwoDecimal(state.expectedValueError * 100)} % )</div>
+                <div>Variance : {roundTwoDecimal(state.variance)} (error = {roundTwoDecimal(state.varianceError * 100)} % )</div>
+
+            </div>
         </Container>
     )
 }
